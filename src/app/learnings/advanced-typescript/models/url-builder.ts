da0e7e9561baf5f93fd3918146a9cc4736345cd2
DangHH <hung.dang@cdtm.de>
2021-05-05 14:05:10 +0200

    adding more boileterplate for test

--- src/app/learnings/advanced-typescript/models/url-builder.ts
+++ src/app/learnings/advanced-typescript/models/url-builder.ts
@@ -1,3 +1,5 @@
+import {Component, Injectable, OnInit} from '@angular/core';
+
 // https://y42.com/dashboard/api/company/{id}/dashboards
 // https://y42.com/dashboard/api/company/{id}/dashboards/{id}
 
@@ -8,7 +10,6 @@
 // https://y42.com/datasource/api/datasources
 // https://y42.com/datasource/api/datasources/{id}
 
-import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -48,5 +49,33 @@ export class DashboardWidgetService extends BaseService {
   providedIn: 'root'
 })
 export class DatasourceService extends BaseService {
+
+}
+
+
+@Component({
+  selector: 'app-url-text',
+  template: `
+    <div></div>
+  `
+})
+export class UrlTestComponent implements OnInit {
+
+  constructor(private dashboardService: DashboardService,
+              private dashboardWidgetService: DashboardWidgetService,
+              private datasourceService: DatasourceService) {
+  }
+
+  getDashoards() {
+
+  }
+
+  getDashoardWidgets() {
+
+  }
+
+  getDatasources() {
+
+  }
 
 }
